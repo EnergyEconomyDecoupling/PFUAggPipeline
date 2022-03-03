fd361708967fbe12291bb6cc1ee188a17af556a4
Matthew Heun <matthew.heun@me.com>
2022-03-03 14:27:48 -0500

    Working on plan.

--- R/agg_plans.R
+++ R/agg_plans.R
@@ -3,24 +3,24 @@ get_plan <- function() {
   list(
     # Set the release that we'll use
     targets::tar_target(
-      PSUT_pin,
+      PSUT_version,
       "20220225T012039Z-c2035"
     ),
 
-    # Establish the pinboard
+    # Set the pinboard folder
     targets::tar_target(
-      PSUT_pinboard,
-      pins::board_folder(PFUSetup::get_abs_paths()[["workflow_releases_folder"]],
-                         versioned = TRUE)
+      pinboard_folder,
+      PFUSetup::get_abs_paths()[["workflow_releases_folder"]],
+      format = "file"),
+
+    # Pull in the PSUT data frame
+    targets::tar_target(
+      PSUT,
+      pins::board_folder(pinboard_folder,
+                         versioned = TRUE) |>
+        pins::pin_read("psut", version = PSUT_version)
     )
   )
 }
 
 
-
-
-# Pull in the PSUT data
-# targets::tar_target(
-#   PSUT,
-#   PSUT_pinboard
-# )
