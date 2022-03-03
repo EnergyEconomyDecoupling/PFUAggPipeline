6f3f9c21ecfce0592d4b089cae1e28ef55c5e6db
Matthew Heun <matthew.heun@me.com>
2022-03-03 13:44:45 -0500

    Building correct _targets.R file.

--- _targets.R
+++ _targets.R
@@ -2,26 +2,8 @@ library(targets)
 # Run targets::tar_make() to run the pipeline
 # and targets::tar_read(summary) to view the results.
 
-summ <- function(dataset) {
-  summarize(dataset, mean_x = mean(x))
-}
-
 # Set target-specific options such as packages.
 tar_option_set(packages = "dplyr")
 
-get_plan <- function() {
-  list(
-    tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-    tar_target(summary, summ(data)) # Call your custom functions as needed.
-  )
-}
 
-
-# End this file with a list of target objects.
-# list(
-#   tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-#   tar_target(summary, summ(data)) # Call your custom functions as needed.
-# )
-
-
-get_plan()
+PFUAggDatabase::get_plan()
