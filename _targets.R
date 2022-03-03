ff87921d9a3a93785c20c51eb9c24ad9d67adef0
Matthew Heun <matthew.heun@me.com>
2022-03-03 16:55:19 -0500

    Add multiprocessor support

--- _targets.R
+++ _targets.R
@@ -1,9 +1,16 @@
 library(targets)
 # Run targets::tar_make() to run the pipeline
-# and targets::tar_read(summary) to view the results.
+# Rung targets::tar_make_clustermq(workers = 8) to execute across multiple cores.
+# and targets::tar_read(<<target_name>>) to view the results.
 
 # Set target-specific options such as packages.
-tar_option_set(packages = "dplyr")
+targets::tar_option_set(
+  packages = "dplyr",
+  resources = tar_resources(
+    clustermq = tar_resources_clustermq(template = list(num_cores = 2))
+  )
+)
+options(clustermq.scheduler = "multiprocess")
 
 
 PFUAggDatabase::get_workflow()
