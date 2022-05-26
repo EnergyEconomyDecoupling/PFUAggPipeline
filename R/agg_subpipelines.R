cc3018cc27e5e11c992b4cf8ef045ffffdec95ae
Matthew Heun <matthew.heun@me.com>
2022-05-26 10:39:37 -0400

    Fixed a referent problem.

--- R/agg_subpipelines.R
+++ R/agg_subpipelines.R
@@ -161,14 +161,16 @@ get_one_middle_pipeline <- function(pr,
       unname(pr)),
 
     # Pull in the PSUT data frame
-    # targets::tar_target_raw("PSUT", quote(pins::board_folder(PinboardFolder, versioned = TRUE) %>%
-    #                                         pins::pin_read("psut", version = PSUTRelease) %>%
-    #                                         filter_countries_and_years(countries = Countries, years = Years))),
+    # targets::tar_target_raw(
+    #   "PSUT",
+    #   quote(pins::board_folder(PinboardFolder, versioned = TRUE) %>%
+    #           pins::pin_read("psut", version = PSUTRelease) %>%
+    #           filter_countries_and_years(countries = Countries, years = Years))),
     targets::tar_target_raw(
       psut_tar_str,
-      quote(pins::board_folder(PinboardFolder, versioned = TRUE) %>%
-              pins::pin_read(PSUTPin, version = PSUTRelease) %>%
-              filter_countries_and_years(countries = Countries, years = Years))),
+      substitute(pins::board_folder(PinboardFolder, versioned = TRUE) %>%
+                   pins::pin_read(psut_tar_sym_pin, version = psut_tar_sym_release) %>%
+                   filter_countries_and_years(countries = Countries, years = Years))),
 
 
     #########################
