fd46034e2112d54532c05dcd0d9c9af67f33e611
Matthew Heun <matthew.heun@me.com>
2022-08-28 21:02:12 -0400

    Add new comment for tar_make

--- _targets.R
+++ _targets.R
@@ -1,6 +1,7 @@
 library(magrittr)
 library(targets)
 # targets::tar_make() to run the pipeline in a single thread.
+# targets::tar_make(callr_function = NULL) to debug.
 # targets::tar_make_future(workers = 8) to execute across multiple cores.
 # targets::tar_read(<<target_name>>) to view the results.
 # targets::tar_destroy() to start over with everything,
