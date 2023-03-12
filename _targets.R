86dd1a1a12d9f794c76a43b2ba23901d366a9b96
Matthew Heun <matthew.heun@me.com>
2023-03-12 14:14:50 -0400

    Now doing all countries and all years. Attempt at 1.0.

--- _targets.R
+++ _targets.R
@@ -11,11 +11,11 @@ library(targets)
 
 # Set the countries to be analyzed.
 # countries <- c("GBR", "USA", "MEX")
-countries <- "USA"
+# countries <- "USA"
 # countries <- "WRLD"
 # countries <- "CHNM"
 # countries <- "all" # Run all countries in the PSUT target.
-# countries <- PFUDatabase::canonical_countries %>% as.character()
+countries <- PFUDatabase::canonical_countries %>% as.character()
 # Countries with unique allocation data plus BEL and TUR (for Pierre).
 # countries <- c("BRA", "CAN", "CHNM", "DEU", "DNK", "ESP", "FRA", "GBR", "GHA",
 #                "GRC", "HKG", "HND", "IDN", "IND", "JOR", "JPN", "KOR", "MEX",
