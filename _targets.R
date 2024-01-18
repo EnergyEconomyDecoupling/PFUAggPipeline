67a9d26b8f6d35e1e956b08ae949d4ff2e0c7188
Matthew Heun <matthew.heun@me.com>
2024-01-17 21:42:58 -0500

    Updated _targets.R to use PFUSetup::pin_version

--- _targets.R
+++ _targets.R
@@ -34,7 +34,8 @@ years <- 1960:2020
 # Tells whether to do the R and Y chops.
 do_chops <- FALSE
 
-setup_version <- "v1.2"
+setup_version <- "v1.3"
+output_version <- "v1.3b1"
 
 # Set the release to be used for input.
 # psut_release <- "20230309T184624Z-7ace5"  # v0.9 (USA only)
@@ -43,18 +44,19 @@ setup_version <- "v1.2"
 # psut_release <- "20230618T131003Z-4c70f"  # v1.1 (with Matrix objects)
 # psut_release <- "20230915T185731Z-c48a0"  # v1.2a1 (Lacks new phi values and updated IEA data)
 # psut_release <- "20230924T185331Z-13381"  # v1.2a2 (Includes new phi values, removes CHNM as RUS exemplar. Lacks updated IEA data)
-psut_release <- "20231207T124854Z-73744"    # v1.2
+# psut_release <- "20231207T124854Z-73744"    # v1.2
 # psut_release <- "20221219T143657Z-964a6"  # For WRLD
 # psut_release <- "20230130T150642Z-631e2"  # For WRLD, 1971
 # psut_release <- "20230130T192359Z-1d3ec"  # For WRLD, 1971-2019
+psut_release <- PFUSetup::pin_versions(output_version)[["psut"]]
 
-psut_without_neu_release <- "20231207T124907Z-bdcbc"    # v1.2 (hopefully) final
-
-phi_vecs_release <- "20231207T124813Z-c8827" # v1.2
-
+# psut_without_neu_release <- "20231207T124907Z-bdcbc"    # v1.2 (hopefully) final
+psut_without_neu_release <- PFUSetup::pin_versions(output_version)[["psut_without_neu"]]
+# phi_vecs_release <- "20231207T124813Z-c8827" # v1.2
+phi_vecs_release <- PFUSetup::pin_versions(output_version)[["phi_vecs"]]
 
 # Should we release the results?
-release <- FALSE
+release <- TRUE
 
 
 
